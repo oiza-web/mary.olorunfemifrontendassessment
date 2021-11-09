2e1bfcc1d76a6bc36aaecfc15e2472275f79d3f0
oiza-web <maryolorunfemi6@gmail.com>
2021-11-09 20:30:29 +0100

    updated checkout screen

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,201 +1,246 @@
 <template>
-<main>
-  <v-row no-gutters>
-      <v-container fill-height >
-
-
+  <main>
+    <v-row no-gutters>
+      <v-container fill-height>
         <v-container class="form__container">
-           <nuxt-link class="mt-6" to="/">
-          <v-img src="/images/tix-logo-mixed2.png" height="50"  contain/>
-        </nuxt-link>
-          <div class="auth__title text-center mb-6 my-8">Checkout</div>
+          <nuxt-link class="mt-6" to="/">
+            <v-img src="/images/tix-logo-mixed2.png" height="50" contain />
+          </nuxt-link>
+          <div class="title text-center mb-6 my-8">Checkout</div>
 
-          <!-- <v-stepper v-model="step">
+          <v-stepper v-model="e1">
             <v-stepper-header>
-              <v-stepper-step :complete="step > 1" step="1"></v-stepper-step>
+              <v-stepper-step :complete="e1 > 1" step="1" color="info">
+                Kindly input your details
+              </v-stepper-step>
+              <v-divider></v-divider>
 
-              <v-divider color="grey"></v-divider>
+              <v-stepper-step :complete="e1 > 2" step="2" color="info">
+                Review your order
+              </v-stepper-step>
 
-              <v-stepper-step :complete="step > 2" step="2"></v-stepper-step>
+              <v-divider></v-divider>
+
+              <v-stepper-step color="info" step="3"> Payment </v-stepper-step>
             </v-stepper-header>
 
             <v-stepper-items>
               <v-stepper-content step="1">
-                <v-form ref="authForm1" v-model="authValid1" class="form__wrapper" @submit.prevent="next">
-                  <v-select :rules="generalRules" :items="genders" item-text="displayName" item-value="name" class="font-weight-bold font__12" :dense="$vuetify.breakpoint.xsOnly" placeholder="I am a" v-model="gender" validate-on-blur />
+                <v-card elevation="0" class="mb-12" color="" height="500px">
+                  <v-form ref="form" v-model="valid" lazy-validation>
+                    <v-text-field
+                      class="my-4"
+                      v-model="name"
+                      color="info"
+                      :rules="nameRules"
+                      label="Name"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      class="my-4"
+                      v-model="number"
+                      color="info"
+                      :rules="numberRules"
+                      label="Phone Number"
+                      required
+                    ></v-text-field>
 
-                  <v-select :rules="generalRules" :items="genders" item-text="displayName" item-value="name" class="font-weight-bold font__12" :dense="$vuetify.breakpoint.xsOnly" placeholder="Looking for" v-model="genderInterest" validate-on-blur />
-                  <v-select :rules="generalRules" :items="genders" item-text="displayName" item-value="name" class="font-weight-bold font__12" :dense="$vuetify.breakpoint.xsOnly" placeholder="Looking for" v-model="genderInterest" validate-on-blur />
-                  <v-select :rules="generalRules" :items="genders" item-text="displayName" item-value="name" class="font-weight-bold font__12" :dense="$vuetify.breakpoint.xsOnly" placeholder="Looking for" v-model="genderInterest" validate-on-blur />
+                    <v-text-field
+                      class="my-4"
+                      color="info"
+                      v-model="email"
+                      :rules="emailRules"
+                      label="E-mail"
+                      required
+                    ></v-text-field>
 
+                    <v-select
+                      class="my-4"
+                      color="info"
+                      v-model="select"
+                      :items="items"
+                      :rules="[(v) => !!v || 'Item is required']"
+                      label="How did you hear about us"
+                      required
+                    ></v-select>
+                  </v-form>
+                </v-card>
 
-                  <v-btn type="submit" depressed block rounded color="primary" height="45" class="mt-12 font-weight-bold">Continue</v-btn>
-                </v-form>
+                <v-btn depressed rounded color="secondary" @click="e1 = 2">
+                  Continue
+                </v-btn>
               </v-stepper-content>
 
               <v-stepper-content step="2">
-                <v-form ref="authForm2" v-model="authValid2" class="form__wrapper" @submit.prevent="signup">
-                  <v-text-field :rules="generalRules" v-model="firstName" placeholder="First Name" class="font__12 font-weight-bold" validate-on-blur />
+                <v-card elevation="0" class="mb-12" color="" height="500px">
+                  <div>
+                    <div class="font-weight-bold font__16 my-4">
+                      Tickect Name - Jhonny Drille Christmas Concert
+                    </div>
+                    <v-img
+                      src="/images/event1.jpg"
+                      max-height="300"
+                      max-width="300"
+                    />
+                    <div class="my-2 font-weight-bold font__16">
+                      RSVP + Shirts
+                    </div>
+                    <v-select
+                      class="my-2"
+                      color="info"
+                      v-model="select"
+                      :items="items2"
+                      :rules="[(v) => !!v || 'Item is required']"
+                      label="Qantity"
+                      required
+                    ></v-select>
+                    <div>
+                      Order summary <v-divider></v-divider>
+                      <p class="font-weight-bold font__20 my-2">Total Price : $1500</p>
+                    </div>
 
-                  <v-text-field :rules="generalRules" v-model="lastName" placeholder="Last Name" class="font__12 font-weight-bold" validate-on-blur />
+                  </div>
+                </v-card>
 
-                  <v-text-field :rules="emailRules" v-model="email" type="email" placeholder="Email" class="font__12 font-weight-bold" validate-on-blur />
+                <v-btn depressed rounded color="secondary" @click="e1 = 3">
+                  Pay Now
+                </v-btn>
 
-                  <v-text-field :rules="generalRules" v-model="phone" type="tel" placeholder="Phone Number" class="font__12 font-weight-bold" validate-on-blur />
-
-                  <v-text-field autocomplete="new-password" :rules="passwordRules" v-model="password" type="password" placeholder="Password" class="font__12 font-weight-bold" validate-on-blur />
-
-                  <v-btn :loading="loading" type="submit" depressed block rounded color="primary" height="45" class="mt-12 font-weight-bold">Checkout</v-btn>
-                </v-form>
+                <v-btn @click="e1 = 2"> Back </v-btn>
               </v-stepper-content>
-            </v-stepper-items>
-          </v-stepper> -->
 
-<v-stepper v-model="e1">
-      <v-stepper-header>
-        <v-stepper-step
-          :complete="e1 > 1"
-          step="1"
-          color="info"
-        >
-          Kindly input your details
-        </v-stepper-step>
+              <v-stepper-content step="3">
+                <v-card
+                  class="mb-12"
+                  color="grey lighten-1"
+                  height="500px"
+                ></v-card>
 
-        <v-divider></v-divider>
+                <v-btn
+                  depressed
+                  rounded
+                  color="secondary"
+                  @click="notificationDialog = true"
+                >
+                  Complete
+                </v-btn>
 
-        <v-stepper-step
-          :complete="e1 > 2"
-          step="2"
-          color="info"
-        >
-          Review your order
-        </v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step color="info" step="3">
-          Payment
-        </v-stepper-step>
-      </v-stepper-header>
-
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <v-card
-          elevation="0"
-            class="mb-12"
-            color=""
-            height="500px"
-          ></v-card>
-
-          <v-btn
-          depressed
-            rounded
-            color="secondary"
-            @click="e1 = 2"
-          >
-            Continue
-          </v-btn>
-
-
-        </v-stepper-content>
-
-        <v-stepper-content step="2">
-          <v-card elevation="0"
-            class="mb-12"
-            color="grey lighten-1"
-            height="500px"
-          ></v-card>
-
-          <v-btn
-          depressed
-            rounded
-            color="secondary"
-            @click="e1 = 3"
-          >
-            Continue
-          </v-btn>
-
-          <!-- <v-btn   @click="e1 = 2">
-            Back
-          </v-btn> -->
-        </v-stepper-content>
-
-        <v-stepper-content step="3">
-          <v-card
-            class="mb-12"
-            color="grey lighten-1"
-            height="500px"
-          ></v-card>
-
-          <v-btn
-          depressed
-            rounded
-            color="secondary"
-            @click="notificationDialog=true"
-          >
-            Complete
-          </v-btn>
-
-          <!-- <v-btn text>
+                <!-- <v-btn text>
            Back
           </v-btn> -->
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
+              </v-stepper-content>
+            </v-stepper-items>
+          </v-stepper>
         </v-container>
-
-
       </v-container>
+    </v-row>
 
-  </v-row>
+    <v-dialog persistent v-model="notificationDialog" max-width="500">
+      <v-sheet class="p__relative">
+        <v-btn
+          @click="notificationDialog = false"
+          light
+          icon
+          class="close__btn white"
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
 
-  <v-dialog persistent v-model="notificationDialog" max-width="500">
-    <v-sheet class="p__relative">
-      <v-btn @click="notificationDialog=false" light icon class="close__btn white">
-        <v-icon>mdi-close</v-icon>
-      </v-btn>
-
-      <div class="text-center px-5 pt-12 pb-7">
-        <div class="title font-weight-bold mb-2">
-          Congratulations there!
+        <div class="text-center px-5 pt-12 pb-7">
+          <div class="title font-weight-bold mb-2">Congratulations there!</div>
+          <div>
+            <v-img src="/images/success-modal2.png" contain />
+          </div>
+          <div class="info--text text--darken-2 font-weight-bold font__14">
+            Kindly check your email your ticket.
+          </div>
         </div>
-        <div>
-          <v-img src="/images/success-modal2.png" contain/>
-        </div>
-        <div class="info--text text--darken-2 font-weight-bold font__14">
-          Kindly check your email your ticket.
-        </div>
-      </div>
 
-      <div class="text-center px-5 pt-7 pb-12 primary">
-        <div class="font-weight-bold white--text mb-5">
-          Thank you for choosing tix.afica
+        <div class="text-center px-5 pt-7 pb-12 primary">
+          <div class="font-weight-bold white--text mb-5">
+            Thank you for choosing tix.afica
+          </div>
+          <v-btn
+            width="150"
+            to="/"
+            depressed
+            rounded
+            color="white"
+            class="black--text font-weight-bold font__12"
+            >View more events</v-btn
+          >
         </div>
-        <v-btn  width="150" to="/" depressed rounded color="white" class="black--text font-weight-bold font__12">View more events</v-btn>
-      </div>
-    </v-sheet>
-
-
-  </v-dialog>
-</main>
+      </v-sheet>
+    </v-dialog>
+  </main>
 </template>
 
 <script>
-
-export default{
-  layout : 'checkout',
-  data(){
-return{
-  e1: 1,
-  notificationDialog: false,
-}
-  }
+export default {
+  layout: 'checkout',
+  data() {
+    return {
+      e1: 1,
+      notificationDialog: false,
+      number: '',
+      name: '',
+      numberRules: [
+        (v) => !!v || 'Phone number is required',
+        (v) => (v && v.length <= 15) || 'Name must be less than 15 characters',
+      ],
+      nameRules: [
+        (v) => !!v || 'Name is required',
+        (v) => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      select: null,
+      items: [
+        'Facebook',
+        'LinkedIn',
+        'Instagram',
+        'Friends & Family',
+        'Celebrity',
+      ],
+      items2: [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+      ],
+      checkbox: false,
+    }
+  },
+  methods: {
+    validate() {
+      this.$refs.form.validate()
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .bg {
-  background: linear-gradient(0deg, rgb(3, 133, 149), rgba(255, 185, 81, 0.7)), url('/images/tix-reviews.png');
+  background: linear-gradient(0deg, rgb(3, 133, 149), rgba(255, 185, 81, 0.7)),
+    url('/images/tix-reviews.png');
   height: 100vh;
   background-size: cover;
   background-position-x: 20%;
@@ -205,7 +250,7 @@ return{
   }
 }
 
-.auth__title {
+.title {
   font-weight: bold;
   font-weight: 700;
   font-size: 30px;
@@ -221,7 +266,7 @@ return{
     height: 4px;
     width: 27px;
     content: '';
-    background-color: #FC6436;
+    background-color: #fc6436;
 
     @media (max-width: 600px) {
       bottom: -10px;
